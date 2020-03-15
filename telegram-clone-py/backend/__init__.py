f746a186224437c03c25237f80355a47d2190b2a
Nick Hackman <snickhackman@gmail.com>
2020-03-15 11:46:29 -0400

    Added support for JWTs for authentication

--- telegram-clone-py/backend/__init__.py
+++ telegram-clone-py/backend/__init__.py
@@ -2,8 +2,10 @@
 import os
 from pathlib import Path
 from typing import Dict, Any, List
+import datetime
 
 import bcrypt
+import jwt
 
 from rest import Rest, Method
 from models import Session, User
@@ -12,6 +14,8 @@ from response import response, Status
 CONFIG_PATH = f"{Path(__file__).parent.absolute()}{os.sep}config.json"
 
 rest: Rest = Rest(CONFIG_PATH)
+
+secret = rest.get_secret()
 
 
 @rest.route("/create/user", Method.POST)
@@ -74,16 +78,49 @@ def login(email: str, payload: Dict[Any, Any]) -> Dict[Any, Any]:
         return response(Status.Error, f"No user with email: {email}")
     if not bcrypt.checkpw(payload["password"].encode(), user.password):
         return response(Status.Failure, f"Password is incorrect")
-    return response(Status.Success, "JWT HERE")
+    return response(
+        Status.Success, create_jwt(user.email, user.password, secret).decode()
+    )
 
 
-@rest.route("/user/delete/<email>/<jwt>", Method.DELETE)
-def delete_user(email: str, jwt: str) -> Dict[Any, Any]:
-    pass
+@rest.route("/user/delete/<email>/<token>", Method.DELETE)
+def delete_user(email: str, token: str) -> Dict[Any, Any]:
+    """
+    Deletes a User
+
+    Parameters
+    ----------
+
+    email: str
+         Email of User to delete
+
+    token: str
+         JWT to authenticate User
+
+    Returns
+    -------
+
+    No User with email: {email}
+    Invalid token
+    Successfully deleted {email}
+    Failed to validate Token
+    """
+    user = Session.query(User).filter(User.email == email).first()
+    if not user:
+        return response(Status.Failure, f"No User with email: {email}")
+    try:
+        json = jwt.decode(token, secret, algorithms=["HS256"])
+        if json["email"] != email and json["password"] == user.password:
+            return response(Status.Error, "Invalid token")
+        Session.delete(user)
+        Session.commit()
+        return response(Status.Success, f"Successfully deleted {email}")
+    except jwt.DecodeError:
+        return response(Status.Error, "Failed to validate token")
 
 
-@rest.route("/update/user/<email>/<jwt>", Method.PUT)
-def update_user(email: str, jwt: str, payload: Dict[Any, Any]) -> Dict[Any, Any]:
+@rest.route("/update/user/<email>/<token>", Method.PUT)
+def update_user(email: str, token: str, payload: Dict[Any, Any]) -> Dict[Any, Any]:
     """
     Updates a User
 
@@ -114,6 +151,10 @@ def update_user(email: str, jwt: str, payload: Dict[Any, Any]) -> Dict[Any, Any]
     user = Session.query(User).filter(User.email == email)
     if not user:
         return response(Status.Failure, f"No User with email: {email}")
+    try:
+        jwt.decode(token, secret, algorithms=["HS256"])
+    except jwt.DecodeError:
+        return response(Status.Error, "Failed to validate token")
     user.update(payload)
     Session.commit()
     return response(Status.Success, user.to_json())
@@ -152,9 +193,43 @@ def list_users() -> Dict[Any, Any]:
     List[str] of User emails
     """
     users: List[User] = Session.query(User).all()
-    print(*users)
     users_json: List[Dict[str, str]] = [user.email for user in users]
     return response(Status.Success, users_json)
+
+
+def create_jwt(email: str, password: bytes, secret: str, duration: int = 24) -> str:
+    """
+    Constructs a JSON Web Token
+
+    Parameters
+    ----------
+
+    email: str
+         User Email address
+
+    password: bytes
+         User encrypted password
+
+    secret: str
+         Server secret passcode
+
+    duration: int
+         Hours to allow defaults to 24
+
+    Returns
+    -------
+
+    A JWT with all this information encrypted
+    """
+    return jwt.encode(
+        {
+            "email": email,
+            "password": password.decode(),
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24),
+        },
+        secret,
+        algorithm="HS256",
+    )
 
 
 rest.run()
